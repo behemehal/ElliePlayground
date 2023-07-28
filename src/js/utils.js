e7914af93c29fcf29666c65ab904a72916ad2559
Ahmetcan Aksu <ahmetcanaksu@behemehal.org>
2023-07-28 03:51:59 +0300

    Add missing modules

---
+++ src/js/utils.js
@@ -0,0 +1,36 @@
+export function messageHandler({ data: { type, message } }) {
+  let colorType = "termWhite";
+
+  switch (type) {
+    case "error":
+    case "error_display":
+      colorType = "termRed";
+      break;
+    case "warning":
+    case "warning_display":
+      colorType = "termYellow";
+      break;
+    case "info":
+      colorType = "termGreen";
+      break;
+    default:
+      colorType = "termWhite";
+      break;
+  }
+  document.querySelector(".bottom_panel").innerHTML +=
+    `<p class="${colorType}">${message}</p>`;
+}
+
+export function stdout(message) {
+  try {
+    messageHandler({ data: JSON.parse(message) });
+    // postMessage(json);
+  } catch (_) {
+    messageHandler({
+      data: {
+        type: "error",
+        message: "Malformed output arrived",
+      },
+    });
+  }
+}
